075223b9dbd5f504a62711ef66f1089b5e73c599
JuneJH <15310487388@163.com>
2021-05-26 23:07:27 +0800

    消息表模型

--- recode/server/src/db/init.ts
+++ recode/server/src/db/init.ts
@@ -1,8 +1,12 @@
 import User from '../models/User';
-
+import Room from '../models/Room'
+import User2Room from '../models/User2Room'
+import Message from '../models/Message'
 async function asyncData() {
     await User.sync({ force: true });
-    console.log("用户模型表刚刚(重新)创建！");
+    await Room.sync({ force: true });
+    await User2Room.sync({ force: true });
+    await Message.sync({ force: true });
+    console.log("模型表刚刚(重新)创建！");
 }
-
 asyncData();
---
+++ recode/server/src/models/Message.ts
@@ -0,0 +1,21 @@
+import { DataTypes } from 'Sequelize';
+import sequelize from '../db/index'
+
+const Message = sequelize.define("message",{
+    id:{
+        type:DataTypes.UUID,
+        primaryKey:true,
+    },
+    content:DataTypes.TEXT,
+    from_id:DataTypes.UUID,
+    to_id:DataTypes.UUID,
+    room_id:DataTypes.UUID,
+    send_time:DataTypes.TIME,
+    from_avatar:DataTypes.STRING,
+    to_avatar:DataTypes.STRING,
+    room_avatar:DataTypes.STRING,
+    from_nickname:DataTypes.STRING,
+    to_nickname:DataTypes.STRING,
+})
+
+export default Message;
